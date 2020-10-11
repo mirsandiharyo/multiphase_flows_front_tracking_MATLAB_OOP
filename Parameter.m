d31f7bb9b0a54985d7712c77040e296ba1d699bb
haryo mirsandi <mirsandiharyo@gmail.com>
2020-10-11 16:00:47 +0200

    Added the parameter class

---
+++ Parameter.m
@@ -0,0 +1,23 @@
+% Domain class.
+classdef Parameter
+    properties(SetAccess = private)
+        nstep
+        dt
+        maxIter
+        maxErr
+        beta
+        outFreq
+    end
+    
+    methods
+        % Initialize the simulation parameters.
+        function obj = Parameter(nstep, dt, maxIter, maxErr, beta, outFreq)
+            obj.nstep = nstep;
+            obj.dt = dt;
+            obj.maxIter = maxIter;
+            obj.maxErr = maxErr;
+            obj.beta = beta;
+            obj.outFreq = outFreq;
+        end
+    end
+end
